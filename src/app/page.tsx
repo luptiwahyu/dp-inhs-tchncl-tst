247d1bce9d7d46f62ac9489008f91ffbf3aa1b39
Lupti Wahyu <luptiwahyu@ymail.com>
2025-12-17 08:26:25 +0700

    feat: add homepage navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-'use client'
-
-import { Provider } from 'react-redux'
-import Pokemon from './pokemon/page'
-import { store } from '@/shared/stores/store'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <Provider store={store}>
-      <Pokemon />
-    </Provider>
+    <div className="h-dvh flex items-center justify-center space-x-4">
+      <Link href="/pokemon" className="hover:text-blue-500">
+        Pokémon
+      </Link>
+      <div>|</div>
+      <Link href="/movies" className="hover:text-blue-500">
+        Movies
+      </Link>
+      <div>|</div>
+      <Link href="/cryptocurrency" className="hover:text-blue-500">
+        Cryptocurrency
+      </Link>
+    </div>
   )
 }
--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,15 +1,19 @@
+'use client'
+
+import { Provider } from 'react-redux'
 import Header from './components/header/header'
 import List from './components/list/list'
 import Title from './components/title/title'
+import { store } from '@/shared/stores/store'
 
 export default function Pokemon() {
   return (
-    <div>
+    <Provider store={store}>
       <Header />
       <main className="px-4 md:px-8 py-6">
         <Title />
         <List />
       </main>
-    </div>
+    </Provider>
   )
 }
